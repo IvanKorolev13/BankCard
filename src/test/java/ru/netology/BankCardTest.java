7cd9f422515c014c319d1ef15faeca2a0469c7b6
Ivan Korolev <ikorolev13@gmail.com>
2023-04-28 13:47:06 +0300

    Ikorolev/ main/ add positive test, task 1

--- src/test/java/ru/netology/BankCardTest.java
+++ src/test/java/ru/netology/BankCardTest.java
@@ -10,28 +10,28 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class BankCardTest {
     private WebDriver driver;
     @BeforeAll
-    public static void setUpAll() {
-//        WebDriverManager.chromedriver().setup();
-        System.setProperty("webdriver.chrome.driver", "/driver/win/chromedriver.exe");
+    static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
+//        System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
     }
     @BeforeEach
-    public void setUpEach() {
+    void setUpEach() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
-//        options.addArguments("--headless");
+        options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.get("http://localhost:9999/");
     }
     @AfterEach
-    public void tearDown() {
+    void tearDown() {
         driver.quit();
         driver = null;
     }
     @Test
     public void testInputValidData() {
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Королев Иван");
-        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+2345678901");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+12345678901");
         driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
         driver.findElement(By.className("button_theme_alfa-on-white")).click();
 
